65e3b5c54ca5e5debcaf37a7da3a237603c943b3
Rafael Godinho <rafael.gslima@gmail.com>
2022-11-01 07:05:33 -0300

    feat: init backend database

---
+++ shareme_backend/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+      {
+        name: 'postedBy',
+        title: 'postedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'save',
+        title: 'Save',
+        type: 'string',
+      }
+    ]
+  }
