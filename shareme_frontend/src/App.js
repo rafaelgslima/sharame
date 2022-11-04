9e3bc44c9e8e2126c4ccc93373997bc8698d2709
Rafael Godinho <rafael.gslima@gmail.com>
2022-11-04 06:46:15 -0300

    feat: create login page

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello World
-        </h1>
-    )
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_ACCESS_KEY}>
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
+    </GoogleOAuthProvider>
+  )
 }
 
 export default App 
---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import decodeJwt from '../helpers/decodeJwt/decodeJwt'
+import { client } from '../client'
+
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem('user', JSON.stringify(decodeJwt(response.credential)));
+
+    const { name, sub, picture } = decodeJwt(response.credential);
+
+    const doc = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture,
+    }
+
+    client.createIfNotExists(doc)
+    .then(() => {
+      navigate('/', { replace: true })
+    })
+  }
+
+  
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className=" relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt='logo' />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              onSuccess={responseGoogle}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
